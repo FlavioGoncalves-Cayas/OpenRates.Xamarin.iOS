d4794e829978eabf54fbe0af4e857d1aa9a69340
Flavio Goncalves <f.goncalves@cayas.de>
2019-11-14 11:49:00 +0100

    Added Intents.h

---
+++ OpenRatesSiriStaticLib/OpenRatesSiriStaticLib/Intents.h
@@ -0,0 +1,16 @@
+//
+//  Intents.h
+//  OpenRatesSiriStaticLib
+//
+//  Created by Flavio Goncalves on 14.11.19.
+//  Copyright © 2019 Cayas Software GmbH. All rights reserved.
+//
+
+#ifndef Intents_h
+#define Intents_h
+
+#import "ConvertAmountToCurrencyIntent.h"
+#import "GetConversionRateIntent.h"
+#import "Currency.h"
+
+#endif /* Intents_h */
