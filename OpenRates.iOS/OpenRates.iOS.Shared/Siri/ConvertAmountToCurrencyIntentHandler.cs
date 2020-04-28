9064ddc8623dee4224fe0a6f6e7788cf758bfc0f
Flavio Goncalves <f.goncalves@cayas.de>
2020-04-28 14:03:24 +0200

    Implemented IntentHandler.GetHandler to return correct IntentHandler for given Intent (+1 squashed commit) Squashed commits: [dbeca16] Implemented IntentHandler.GetHandler to return correct IntentHandler for given Intent

---
+++ OpenRates.iOS/OpenRates.iOS.Shared/Siri/ConvertAmountToCurrencyIntentHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ObjCRuntime;
+using OpenRates.iOS.Siri;
+
+namespace OpenRates.iOS.Shared.Siri
+{
+    public class ConvertAmountToCurrencyIntentHandler : ConvertAmountToCurrencyIntentHandling
+    {
+        public ConvertAmountToCurrencyIntentHandler()
+        {
+        }
+
+        public override void HandleConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyIntentResponse> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveAmountForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyAmountResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveTargetCurrencyForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyTargetCurrencyResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OpenRates.iOS/OpenRates.iOS.SiriExtension/IntentHandler.cs
+++ OpenRates.iOS/OpenRates.iOS.SiriExtension/IntentHandler.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
+using OpenRates.iOS.Siri;
 
 using Foundation;
 using Intents;
+using OpenRates.iOS.Shared.Siri;
 
 namespace OpenRates.iOS.SiriExtension
 {
-    // As an example, this class is set up to handle Message intents.
-    // You will want to replace this or add other intents as appropriate.
-    // The intents you wish to handle must be declared in the extension's Info.plist.
-
-    // You can test your example integration by saying things to Siri like:
-    // "Send a message using <myApp>"
-    // "<myApp> John saying hello"
-    // "Search for messages in <myApp>"
     [Register("IntentHandler")]
     public class IntentHandler : INExtension
     {
         protected IntentHandler(IntPtr handle) : base(handle)
         {
-            // Note: this .ctor should not contain any initialization logic.
         }
 
         public override NSObject GetHandler(INIntent intent)
         {
-            // This is the default implementation.  If you want different objects to handle different intents,
-            // you can override this and return the handler you want for that particular intent.
+            if (intent is ConvertAmountToCurrencyIntent)
+                return new ConvertAmountToCurrencyIntentHandler();
 
-            return this;
+            if (intent is GetConversionRateIntent)
+                return new GetConversionRateIntentHandler();
+        
+            return null;
         }
     }
 }
