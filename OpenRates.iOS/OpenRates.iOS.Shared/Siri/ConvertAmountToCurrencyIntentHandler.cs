cc93b2191bf279355526ab3987d9f77f784d851a
Flavio Goncalves <f.goncalves@cayas.de>
2020-05-12 17:15:52 +0200

    Implemented ConvertAmountToCurrencyIntentHandler and GetConversionRateIntentHandler

--- OpenRates.iOS/OpenRates.iOS.Shared/Siri/ConvertAmountToCurrencyIntentHandler.cs
+++ OpenRates.iOS/OpenRates.iOS.Shared/Siri/ConvertAmountToCurrencyIntentHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using ObjCRuntime;
+using System.Globalization;
+using OpenRates.Api;
+using OpenRates.iOS.Shared.Model;
 using OpenRates.iOS.Siri;
 
 namespace OpenRates.iOS.Shared.Siri
@@ -12,22 +14,81 @@ namespace OpenRates.iOS.Shared.Siri
 
         public override void HandleConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyIntentResponse> completion)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var knownBaseCurrency = Enum.Parse<KnownCurrency>(intent.BaseCurrency.ToString().ToUpper());
+                var knownTargetCurrency = Enum.Parse<KnownCurrency>(intent.TargetCurrency.ToString().ToUpper());
+
+                var response = new OpenRatesClient().GetLatestWithBaseAsync(knownBaseCurrency.ToString()).GetAwaiter().GetResult();
+
+                var result = Math.Round(response.Rates[knownTargetCurrency.ToString()] * intent.Amount.DoubleValue, 2);
+
+                var nsAmount = new Foundation.NSDecimalNumber(result.ToString(CultureInfo.InvariantCulture));
+                var nCurrencyAmount = new Intents.INCurrencyAmount(nsAmount, knownTargetCurrency.ToString());
+
+                completion(ConvertAmountToCurrencyIntentResponse.SuccessIntentResponseWithBaseAmount(intent.Amount, intent.BaseCurrency, nCurrencyAmount));
+            }
+            catch(Exception e)
+            {
+                completion(new ConvertAmountToCurrencyIntentResponse(ConvertAmountToCurrencyIntentResponseCode.Failure, null));
+            }
         }
 
         public override void ResolveAmountForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyAmountResolutionResult> completion)
         {
-            throw new NotImplementedException();
+            if(intent.Amount == null)
+            {
+                completion(ConvertAmountToCurrencyAmountResolutionResult.NeedsValue());
+
+                return;
+            }
+
+            if(intent.Amount.DoubleValue < 0)
+            {
+                completion(ConvertAmountToCurrencyAmountResolutionResult.UnsupportedForReason(ConvertAmountToCurrencyAmountUnsupportedReason.NegativeNumbersNotSupported));
+
+                return;
+            }
+
+            completion(ConvertAmountToCurrencyAmountResolutionResult.SuccessWithResolvedValue(intent.Amount.DoubleValue));
         }
 
         public override void ResolveBaseCurrencyForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyBaseCurrencyResolutionResult> completion)
         {
-            throw new NotImplementedException();
+            if(intent.BaseCurrency == Currency.Unknown)
+            {
+                completion(ConvertAmountToCurrencyBaseCurrencyResolutionResult.NeedsValue());
+
+                return;
+            }
+
+            if(!Enum.TryParse<KnownCurrency>(intent.BaseCurrency.ToString().ToUpper(), out _))
+            {
+                completion(ConvertAmountToCurrencyBaseCurrencyResolutionResult.Unsupported());
+
+                return;
+            }
+
+            completion(ConvertAmountToCurrencyBaseCurrencyResolutionResult.SuccessWithResolvedCurrency(intent.BaseCurrency));
         }
 
         public override void ResolveTargetCurrencyForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyTargetCurrencyResolutionResult> completion)
         {
-            throw new NotImplementedException();
+            if (intent.TargetCurrency == Currency.Unknown)
+            {
+                completion(ConvertAmountToCurrencyTargetCurrencyResolutionResult.NeedsValue());
+
+                return;
+            }
+
+            if (!Enum.TryParse<KnownCurrency>(intent.TargetCurrency.ToString().ToUpper(), out _))
+            {
+                completion(ConvertAmountToCurrencyTargetCurrencyResolutionResult.Unsupported());
+
+                return;
+            }
+
+            completion(ConvertAmountToCurrencyTargetCurrencyResolutionResult.SuccessWithResolvedCurrency(intent.TargetCurrency));
         }
     }
 }
--- OpenRates.iOS/OpenRates.iOS.Shared/Siri/GetConversionRateIntentHandler.cs
+++ OpenRates.iOS/OpenRates.iOS.Shared/Siri/GetConversionRateIntentHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using ObjCRuntime;
+using OpenRates.Api;
+using OpenRates.iOS.Shared.Model;
 using OpenRates.iOS.Siri;
 
 namespace OpenRates.iOS.Shared.Siri
@@ -12,17 +14,59 @@ namespace OpenRates.iOS.Shared.Siri
 
         public override void HandleGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateIntentResponse> completion)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var knownBaseCurrency = Enum.Parse<KnownCurrency>(intent.BaseCurrency.ToString().ToUpper());
+                var knownTargetCurrency = Enum.Parse<KnownCurrency>(intent.TargetCurrency.ToString().ToUpper());
+
+                var response = new OpenRatesClient().GetLatestWithBaseAsync(knownBaseCurrency.ToString()).GetAwaiter().GetResult();
+
+                var result = response.Rates[knownTargetCurrency.ToString()];
+                
+                completion(GetConversionRateIntentResponse.SuccessIntentResponseWithBaseCurrency(intent.BaseCurrency, intent.TargetCurrency, new Foundation.NSNumber(result)));
+            }
+            catch (Exception e)
+            {
+                completion(new GetConversionRateIntentResponse(GetConversionRateIntentResponseCode.Failure, null));
+            }
         }
 
         public override void ResolveBaseCurrencyForGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateBaseCurrencyResolutionResult> completion)
         {
-            throw new NotImplementedException();
+            if (intent.BaseCurrency == Currency.Unknown)
+            {
+                completion(GetConversionRateBaseCurrencyResolutionResult.NeedsValue());
+
+                return;
+            }
+
+            if (!Enum.TryParse<KnownCurrency>(intent.BaseCurrency.ToString().ToUpper(), out _))
+            {
+                completion(GetConversionRateBaseCurrencyResolutionResult.Unsupported());
+
+                return;
+            }
+
+            completion(GetConversionRateBaseCurrencyResolutionResult.SuccessWithResolvedCurrency(intent.BaseCurrency));
         }
 
         public override void ResolveTargetCurrencyForGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateTargetCurrencyResolutionResult> completion)
         {
-            throw new NotImplementedException();
+            if (intent.TargetCurrency == Currency.Unknown)
+            {
+                completion(GetConversionRateTargetCurrencyResolutionResult.NeedsValue());
+
+                return;
+            }
+
+            if (!Enum.TryParse<KnownCurrency>(intent.TargetCurrency.ToString().ToUpper(), out _))
+            {
+                completion(GetConversionRateTargetCurrencyResolutionResult.Unsupported());
+
+                return;
+            }
+
+            completion(GetConversionRateTargetCurrencyResolutionResult.SuccessWithResolvedCurrency(intent.TargetCurrency));
         }
     }
 }
