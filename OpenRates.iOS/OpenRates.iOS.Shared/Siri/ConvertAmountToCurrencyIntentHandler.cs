d11a4a4931b7fa0b212bc73c62b72ac9a71ed5a6
Flavio Goncalves <f.goncalves@cayas.de>
2020-05-12 16:51:45 +0200

    Implemented IntentHandler.cs and added intent-specific handler-classes

---
+++ OpenRates.iOS/OpenRates.iOS.Shared/Siri/ConvertAmountToCurrencyIntentHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ObjCRuntime;
+using OpenRates.iOS.Siri;
+
+namespace OpenRates.iOS.Shared.Siri
+{
+    public class ConvertAmountToCurrencyIntentHandler : ConvertAmountToCurrencyIntentHandling
+    {
+        public ConvertAmountToCurrencyIntentHandler()
+        {
+        }
+
+        public override void HandleConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyIntentResponse> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveAmountForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyAmountResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveBaseCurrencyForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyBaseCurrencyResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveTargetCurrencyForConvertAmountToCurrency(ConvertAmountToCurrencyIntent intent, Action<ConvertAmountToCurrencyTargetCurrencyResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenRates.iOS/OpenRates.iOS.Shared/Siri/GetConversionRateIntentHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ObjCRuntime;
+using OpenRates.iOS.Siri;
+
+namespace OpenRates.iOS.Shared.Siri
+{
+    public class GetConversionRateIntentHandler : GetConversionRateIntentHandling
+    {
+        public GetConversionRateIntentHandler()
+        {
+        }
+
+        public override void HandleGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateIntentResponse> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveBaseCurrencyForGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateBaseCurrencyResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResolveTargetCurrencyForGetConversionRate(GetConversionRateIntent intent, Action<GetConversionRateTargetCurrencyResolutionResult> completion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
